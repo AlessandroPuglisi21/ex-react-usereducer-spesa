240fdf2cdd8bb4226bd54a2bdde90d4444ca7504
AlessandroPuglisi <alessandropuglisi2000@gmail.com>
2025-03-17 15:33:56 +0100

    ADD:Milestone 2

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 function ListOfProducts() {
     const products = [
         { name: 'Mela', price: 0.5 },
@@ -6,12 +8,35 @@ function ListOfProducts() {
         { name: 'Pasta', price: 0.7 },
     ];
 
+    const [addedProducts, setAddedProducts] = useState([]);
+
+    const [carrello, setCarrello] = useState(0);
+
+    function addToCart(product) {
+        if (!addedProducts.some(p => p.name === product.name)) {
+            setAddedProducts([...addedProducts, product]);
+            setCarrello(carrello + 1);
+        }
+    }
+
     return (
         <>
+            <h3>
+                Carrello = {carrello}
+            </h3>
             <h1>Lista Prodotti</h1>
             <div>
                 <ul>
                     {products.map((product, index) => (
+                        <li key={index}>
+                            {product.name} - {product.price} €
+                            <button onClick={() => addToCart(product)}>Aggiungi al carrello</button>
+                        </li>
+                    ))}
+                </ul>
+                <h1>Prodotti nel Carrello</h1>
+                <ul>
+                    {addedProducts.map((product, index) => (
                         <li key={index}>
                             {product.name} - {product.price} €
                         </li>
